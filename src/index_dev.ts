423abec80fdaaca9ed8af48ffd6713e2fc9d3269
Tam Nhu Tran <kaitran.ntt@gmail.com>
2024-02-08 20:03:24 -0500

    Prettier & wrapping in database connection establishment

--- src/index_dev.ts
+++ src/index_dev.ts
@@ -1,11 +1,11 @@
 import { createConnection } from "typeorm";
 import express from "express";
 import * as bodyParser from "body-parser";
-import cors from 'cors';
+import cors from "cors";
 
-import * as dotenv from 'dotenv';
+import * as dotenv from "dotenv";
 dotenv.config({
-    path: './.env.dev'
+  path: "./.env.dev",
 });
 
 import logger from "./utils/logger";
@@ -15,15 +15,24 @@ import { PORT_NUMBER } from "./config";
 //  const  { router } = require("./services");
 import router from "./services/routes";
 
-// create express app
-const app = express();
-app.use(bodyParser.json());
-app.use(cors());
+// Attempt to establish a database connection before starting the server
+createConnection()
+  .then(async (connection) => {
+    const app = express();
 
-// Incorrectly way to mount the router at the root path ("/")
-// app.set('/', router);
-app.use("/", router);
+    // create express app
+    app.use(bodyParser.json());
+    app.use(cors());
+    // app.use(logger);
 
-app.listen(PORT_NUMBER);
+    // Incorrectly way to mount the router at the root path ("/")
+    // app.set('/', router);
+    app.use("/", router);
 
-console.log(`Solmate Presale API has started on ${PORT_NUMBER} port.`);
+    app.listen(PORT_NUMBER, () => {
+      console.log(`Solmate Presale API has started on port ${PORT_NUMBER}.`);
+    });
+  })
+  .catch((error) => {
+    console.error("Error connecting to the database: ", error);
+  });
